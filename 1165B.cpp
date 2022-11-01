64d5b487ca5325fec6022f0dd67cc5eb0ff94ee5
Анастасия <71327963+anasteisha-4@users.noreply.github.com>
2022-11-01 15:58:35 +0300

    Add files via upload

---
+++ 1165B.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+	int n, k, res = 0;
+	bool fl;
+	std::vector <int> tasks;
+	std::cin >> n;
+	for (int i = 0; i < n; i++) {
+		std::cin >> k;
+		tasks.push_back(k);
+	}
+	std::sort(tasks.begin(), tasks.end());
+	for (int i = 0; i < tasks.size(); i++) {
+		fl = false;
+		for (int j = i; j < tasks.size(); j++) {
+			if (i + 1 <= tasks[j]) {
+				res++;
+				fl = true;
+				tasks.erase(tasks.begin() + j);
+				break;
+			}
+		}
+		if (!fl) break;
+	}
+	std::cout << res;
+	return 0;
+}
