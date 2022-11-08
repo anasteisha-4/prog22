fd8b46a0c20f88a0086fb7f6b9de819ff008d820
Анастасия <71327963+anasteisha-4@users.noreply.github.com>
2022-11-08 16:08:54 +0300

    Add files via upload

---
+++ 1017A.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <array>
+#include <algorithm>
+
+using namespace std;
+
+bool comp(std::array <int, 2> a, std::array <int, 2> b) {
+	return (a[1] > b[1]);
+}
+
+int main()
+{
+	int n, a, b, c, d, ans;
+	std::cin >> n;
+	std::array <array <int, 2>, 1000> mas;
+	std::cin >> a >> b >> c >> d;
+	int suum = a + b + c + d;
+	mas[0] = { 1, a + b + c + d };
+	for (int i = 1; i < n; i++) {
+		std::cin >> a >> b >> c >> d;
+		mas[i] = { i + 1, a + b + c + d };
+	}
+	std::sort(mas.begin(), mas.end(), comp);
+	for (int i = 0; i < n; i++) {
+		if (mas[i][0] == 1) {
+			ans = i + 1;
+			break;
+		}
+	}
+	std::cout << ans;
+	return 0;
+}
